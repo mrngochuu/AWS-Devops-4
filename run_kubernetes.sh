6cc2b52b853d66617342c86888df3d54de4e5e3a
Huu Do <ngochuu.bts@gmail.com>
2022-12-24 18:28:17 +0700

    Run Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,22 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=mrngochuu/kubenetes-predict
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+rpod=$(kubectl get pod kubenetes-predict 2> /dev/null;echo $?)
+if [ "_$rpod" == "_1" ] ; then
+        kubectl run kubenetes-predict \
+        --image=$dockerpath \
+        --image-pull-policy="Always" \
+        --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "secretkey"}]}}'
+fi
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward kubenetes-predict 8080:80
